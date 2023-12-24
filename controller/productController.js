62d7daed9cb2f8cd8c3b7b606c1ddaa12f1d50f9
Metyme1 <millionmety@gmail.com>
2023-12-24 10:40:06 -0300

    async error handler added

--- controller/productController.js
+++ controller/productController.js
@@ -1,40 +1,43 @@
 
 const Product = require ('../model/productmodel')
+const asyncHandler = require('express-async-handler')
 // get all product 
-const getProducts =async(req,res)=>{
+const getProducts =asyncHandler(async(req,res)=>{
     try{
 
         const products = await Product.find({});
         res.status(200).json(products)
     }catch(error){
-        res.status(500).json({message:error.message})
+        res.status (500)
+        throw new Error(error.message)
     }
-}
+})
 // get product by id
-const getProduct = async(req,res)=>{
+const getProduct = asyncHandler(async(req,res)=>{
     try{
         const {id}= req.params;
         const product = await Product.findById(id);
         res.status(200).json(product)
     }catch(error){
-        res.status(500).json({message:error.message})
+        res.status (500)
+        throw new Error(error.message)
     }
-}
+})
 // post product
 
-const postproduct = async (req,res)=>{
+const postproduct = asyncHandler(async (req,res)=>{
     try{
      const product = await Product.create(req.body)
      res.status(200).json(product)
  
     } catch (error){
-     console.log(error.message);
-     res.status(500).json({message:error.message})
+        res.status (500)
+        throw new Error(error.message)
     }
- }
+ })
  // update
 
- const updateProduct = async (req, res) => {
+ const updateProduct = asyncHandler(async (req, res) => {
 
 
     try{
@@ -47,12 +50,13 @@ const postproduct = async (req,res)=>{
         res.status(200).json(updateProduct);
     }
     catch(error){
-        res.status(500).json({message:error.message})
+        res.status (500)
+        throw new Error(error.message)
     }
-}
+})
 // delete
 
-const deleteProduct = async (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
     try {
         const { id } = req.params;
         const product = await Product.findByIdAndDelete(id);
@@ -63,7 +67,7 @@ const deleteProduct = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+})
 
 module.exports ={
     getProduct,
